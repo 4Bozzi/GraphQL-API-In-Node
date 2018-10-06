aa96f3a8488b5b2b40bd1721a38b7d67eec9331f
Andrew Bozzi <4bozzi@gmail.com>
2018-10-06 15:21:10 -0400

    Initial setup

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
-// Declare an app from express
-const app = null
+import express from 'express';
+import setupMiddware from './middleware';
+import { restRouter } from './api';
+import { connect } from './db';
+import { signin, protect } from './api/modules/auth';
 
-export default app
+// Declare an app from express
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ ok: true });
+});
+
+export default app;
