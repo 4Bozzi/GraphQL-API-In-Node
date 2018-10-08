23e324b4d74555b4822fb96f084742231897f61a
Andrew Bozzi <4bozzi@gmail.com>
2018-10-07 20:17:45 -0400

    Revert "Setting up basic routing."
    This reverts commit 18819397595c0d1cbe4b75537f7def141d6137a5.

--- src/db.js
+++ src/db.js
@@ -1,11 +1,8 @@
-import mongoose from 'mongoose';
-mongoose.Promise = global.Promise;
+import mongoose from 'mongoose'
+mongoose.Promise = global.Promise
 
 export const connect = () => {
-  return mongoose.connect(
-    'https://mlab.com/databases/learner',
-    {
-      useMongoClient: true
-    }
-  );
-};
+  return mongoose.connect('mongodb://localhost/jams', {
+    useMongoClient: true
+  })
+}
--- src/server.js
+++ src/server.js
@@ -3,26 +3,11 @@ import setupMiddware from './middleware';
 import { restRouter } from './api';
 import { connect } from './db';
 import { signin, protect } from './api/modules/auth';
-<<<<<<< Updated upstream
 
 // Declare an app from express
 const app = express();
 
 app.get('/', (req, res) => {
-=======
-// Declare an app from express
-const app = express();
-
-setupMiddware(app);
-connect();
-// setup basic routing for index route
-
-app.use('/signin', signin);
-app.use('/api', restRouter);
-
-// catch all
-app.all('*', (req, res) => {
->>>>>>> Stashed changes
   res.json({ ok: true });
 });
 
