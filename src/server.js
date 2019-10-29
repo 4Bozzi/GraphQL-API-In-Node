f7d305252f09d8c2e755cb0bd868a5d03fa7a897
Andrew Bozzi <4Bozzi@users.noreply.github.com>
2019-10-29 19:39:45 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,24 +4,21 @@ import { restRouter } from './api';
 import { connect } from './db';
 import { signin, protect } from './api/modules/auth';
 
-// Declare an app from express
 const app = express();
 
 setupMiddware(app);
 connect();
-// setup basic routing for index route
 
 app.use('/signin', signin);
 app.use('/api', protect, restRouter);
-// catch all
+
 app.all('*', (req, res) => {
 =======
 app.use('/api', restRouter);
-// catch all
+
 app.all('*', (req, res) => {
 =======
 
-// Declare an app from express
 const app = express();
 
 app.get('/', (req, res) => {
